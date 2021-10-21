3e9849db9f6dca99a4fbbbeb88e80920ec45f38f
Jizu Sun <jizu.sun@philips.com>
2021-10-21 13:27:50 +0800

    finish

--- cmd/goroutine-network-request/main.go
+++ cmd/goroutine-network-request/main.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 	"sync"
 )
 
 func main() {
 	var wg sync.WaitGroup
-	for i := 0; i < 3; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			fmt.Println("hello")
-		}()
+
+	limit, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	url := os.Args[2]
+	wg.Add(limit)
+
+	for i := 0; i < limit; i++ {
+		go get(i, &wg, url)
 	}
 	wg.Wait()
+	fmt.Println("end")
+}
+
+func get(num int, wg *sync.WaitGroup, url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(url, resp.StatusCode, len(body))
+	wg.Done()
 }
